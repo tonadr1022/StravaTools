91a6e85daad0a397bda4a055b17970918324e8bf
Tony Adriansen <tony.adriansen@gmail.com>
2023-10-06 07:50:45 -0500

    prisma generate fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,13 @@
 import { getServerSession } from "next-auth/next";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const d = await getServerSession(authOptions);
-  const id = d?.user.id;
+  const { user } = await getServerSession(authOptions);
+  const user_id = user.id;
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>test: {id}</div>
+      <div>test: {user_id}</div>
       <button>Authorize Strava</button>
     </main>
   );
