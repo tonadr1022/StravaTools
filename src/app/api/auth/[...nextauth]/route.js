0ff1c4ab75ccee9fcd80d869808e25b7af16fbc1
Tony Adriansen <tony.adriansen@gmail.com>
2023-10-06 07:31:33 -0500

    first

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    session: async ({ session, user }) => {
+      if (session?.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+  adapter: PrismaAdapter(prisma),
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
